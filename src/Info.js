cfb694f446f497af4c0ba7dd0ca2681f3cb4111f
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-09 15:27:30 +0900

    useEffect

--- src/Info.js
+++ src/Info.js
@@ -1,9 +1,15 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
 const Info = () => {
   //component명의 첫번째가 대문자가 아닐 경우 useState에서 오류가 난다.
   const [name, setName] = useState("");
   const [nickname, setNickname] = useState("");
+
+  //name,nickname 입력 시 console.log에 실시간으로 렌더링되었다고 표시된다.
+  useEffect(() => {
+    console.log("렌더링이 완료되었습니다.");
+    console.log({name, nickname});
+  });
 
   const onChangeName = e => {
     setName(e.target.value); //e.target.value는 인풋태그에 입력되는 값
